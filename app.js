fc12ffb9c6df2968bb68967d5879582bd2f7de1d
Ronei Gualberto <ronei.mac.rmg@gmail.com>
2018-01-28 14:18:56 -0400

    Trabalhando com export ES6

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import { union, uniq } from 'ramda';
+import soma, { sub, mult, div} from './utils';
 
 
 const arr1 = [1, 1, 1, 2, 2, 3, 4, 5, 6, 6];
@@ -10,3 +11,8 @@ const arr4 = uniq(arr1, arr2);
 
 console.log(arr3);
 console.log(arr4)
+
+console.log(soma(2, 2));
+console.log(sub(2, 2));
+console.log(mult(2, 2));
+console.log(div(2, 2));
---
+++ utils.js
@@ -0,0 +1,21 @@
+
+function sum(a, b) {
+    return a + b;
+}
+
+export function sub(a, b) {
+    return a - b;
+}
+
+function mult(a, b) {
+    return a * b;
+}
+
+function div(a, b) {
+    return a / b;
+}
+
+export {mult, div};
+
+export default sum;
+
